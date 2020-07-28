915145cfa791d8449c278669a957e59d52f00f02
Maxfield Herman <leawoodmax@yahoo.com>
2020-07-28 15:03:34 -0400

    condensed testing structure

--- enforce_rules.test.js
+++ enforce_rules.test.js
@@ -1,55 +1,28 @@
-let {allowMessage, rule1Evaluation, rule4Evaluation} = require("./enforce-rules");
+let {rule1Evaluation, rule4Evaluation} = require("./enforce-rules");
 
 // RULE1EVALUATION
 describe('rule1Evaluation', () => {
     const data = {
-      roundId: "true",
-      speaker: "Human",
-      addressee: "Watson",
-      text: "Watson, I will buy 3 eggs for $4",
-      role: "buyer",
-      timestamp: 1595945030581,
+        roundId: "true",
+        speaker: "Human",
+        addressee: "Watson",
+        text: "Watson, I will buy 3 eggs for $4",
+        role: "buyer",
+        timestamp: 1595945030581,
     };
-    // Test if "accept" message passes
-    test('accept pass', () => {
-        const testData = Object.assign({}, data, {bid: {"type": "Accept", "price": {"value": 10}}});
-        expect(rule1Evaluation(testData, 50)).toEqual({permit: true, rationale: null});
-    });
 
-    // Test if non-accept message passes
-    test('non-accept pass', () => {
-        const testData = Object.assign({}, data, {bid: {"type": "Reject", "price": {"value": 10}}});
-        expect(rule1Evaluation(testData, 50)).toEqual({permit: true, rationale: null});
-    });
-
-    // Test if value, type not given
-    test('no type given pass', () => {
-        const testData = Object.assign({}, data, {bid: {"price": {"value": 10}}});
-        expect(rule1Evaluation(testData, 50)).toEqual({permit: true, rationale: null});
-    });
-
-    // humanbudget > bidamount
-    test('greater amount pass', () => {
-        const testData = Object.assign({}, data, {bid: {"type": "Accept", "price": {"value": 49}}});
-        expect(rule1Evaluation(testData, 50)).toEqual({permit: true, rationale: null});
-    });
-
-    // humanbudget === bidamount
-    test('budget = bid pass', () => {
-        const testData = Object.assign({}, data, {bid: {"type": "Accept", "price": {"value": 50}}});
-        expect(rule1Evaluation(testData, 50)).toEqual({permit: true, rationale: null});
-    });
-
-    // humanbudget < bidamount
-    test('bid > budget pass', () => {
-        const testData = Object.assign({}, data, {bid: {"type": "Accept", "price": {"value": 51}}});
-        expect(rule1Evaluation(testData, 50)).toEqual({permit: false, rationale: "Insufficient budget"});
-    });
-
-    // test if function fails if no bid value given
-    test('no bid value pass', () => {
-        const testData = Object.assign({}, data, {bid: {"type": "Accept"}});
-        expect(rule1Evaluation(testData, 50)).toEqual({permit: true, rationale: null});
+    test.each([
+        [{"type": "Accept", "price": {"value": 10}}, 50, {permit: true, rationale: null}],
+        [{"type": "Reject", "price": {"value": 10}}, 50, {permit: true, rationale: null}],
+        [{"price": {"value": 10}}, 50, {permit: true, rationale: null}],
+        [{"type": "Accept", "price": {"value": 49}}, 50, {permit: true, rationale: null}],
+        [{"type": "Accept", "price": {"value": 50}}, 50, {permit: true, rationale: null}],
+        [{"type": "Accept", "price": {"value": 51}}, 50, {permit: false, rationale: "Insufficient budget"}],
+        [{"type": "Accept"}, 50, {permit: true, rationale: null}]
+    ])("Given bid %s and budget %s, expect %s", (bid, budget, expected) => {
+        test.name = "abc";
+        const testData = Object.assign({}, data, {bid});
+        expect(rule1Evaluation(testData, budget)).toEqual(expected);
     });
 });
 
