08fdcd21ed3d0c17018e7a5821b631399a3a2e76
Maxfield Herman <leawoodmax@yahoo.com>
2020-07-28 15:45:12 -0400

    changed %s to %j for json objects

--- enforce_rules.test.js
+++ enforce_rules.test.js
@@ -61,7 +61,7 @@ describe('rule4Evaluation', () => {
         [{"text": "a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v"}, {permit: true, rationale: null}],
         [{"text": "a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w"}, {permit: false, rationale: "Excessive message length"}],
         [{}, {permit: true, rationale: null}]
-    ])(`Given text %s, expect %s`, (text, expected) => {
+    ])(`Given text %s, expect %j`, (text, expected) => {
         expect(rule4Evaluation(text)).toEqual(expected);
     });
 });
