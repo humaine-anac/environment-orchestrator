8bc8d00b252317b4c952f2c91e81f493d86c82df
Maxfield Herman <leawoodmax@yahoo.com>
2020-07-28 14:10:36 -0400

    modified test structure

--- enforce_rules.test.js
+++ enforce_rules.test.js
@@ -1,49 +1,75 @@
 let {allowMessage, rule1Evaluation, rule4Evaluation} = require("./enforce-rules");
-let test_data = require("./enforce_rules_test_data.json");
 
 // RULE1EVALUATION
-// Test if "accept" message passes
-test('rule1 accept pass', () => {
-    expect(rule1Evaluation(test_data.rule1.test1, 50)).toEqual({permit: true, rationale: null});
-});
+describe('rule1Evaluation', () => {
+    const data = {
+      roundId: "true",
+      speaker: "Human",
+      addressee: "Watson",
+      text: "Watson, I will buy 3 eggs for $4",
+      role: "buyer",
+      timestamp: 1595945030581,
+    };
+    // Test if "accept" message passes
+    test('accept pass', () => {
+        const testData = Object.assign({}, data, {bid: {"type": "Accept", "price": {"value": 10}}});
+        expect(rule1Evaluation(testData, 50)).toEqual({permit: true, rationale: null});
+    });
 
-// Test if non-accept message passes
-test('rule1 non-accept pass', () => {
-    expect(rule1Evaluation(test_data.rule1.test2, 50)).toEqual({permit: true, rationale: null});
-});
+    // Test if non-accept message passes
+    test('non-accept pass', () => {
+        const testData = Object.assign({}, data, {bid: {"type": "Reject", "price": {"value": 10}}});
+        expect(rule1Evaluation(testData, 50)).toEqual({permit: true, rationale: null});
+    });
 
-// Test if value, type not given
-test('rule1 no type given pass', () => {
-    expect(rule1Evaluation(test_data.rule1.test3, 50)).toEqual({permit: true, rationale: null});
-});
+    // Test if value, type not given
+    test('no type given pass', () => {
+        const testData = Object.assign({}, data, {bid: {"price": {"value": 10}}});
+        expect(rule1Evaluation(testData, 50)).toEqual({permit: true, rationale: null});
+    });
 
-// humanbudget > bidamount
-test('rule1 greater amount pass', () => {
-    expect(rule1Evaluation(test_data.rule1.test4, 50)).toEqual({permit: true, rationale: null});
-});
+    // humanbudget > bidamount
+    test('greater amount pass', () => {
+        const testData = Object.assign({}, data, {bid: {"type": "Accept", "price": {"value": 49}}});
+        expect(rule1Evaluation(testData, 50)).toEqual({permit: true, rationale: null});
+    });
 
-// humanbudget === bidamount
-test('rule1 budget = bid pass', () => {
-    expect(rule1Evaluation(test_data.rule1.test5, 50)).toEqual({permit: true, rationale: null});
-});
+    // humanbudget === bidamount
+    test('budget = bid pass', () => {
+        const testData = Object.assign({}, data, {bid: {"type": "Accept", "price": {"value": 50}}});
+        expect(rule1Evaluation(testData, 50)).toEqual({permit: true, rationale: null});
+    });
 
-// humanbudget < bidamount
-test('rule1 bid > budget pass', () => {
-    expect(rule1Evaluation(test_data.rule1.test6, 50)).toEqual({permit: false, rationale: "Insufficient budget"});
-});
+    // humanbudget < bidamount
+    test('bid > budget pass', () => {
+        const testData = Object.assign({}, data, {bid: {"type": "Accept", "price": {"value": 51}}});
+        expect(rule1Evaluation(testData, 50)).toEqual({permit: false, rationale: "Insufficient budget"});
+    });
 
-// test if function fails if no bid given
-test('rule1 no bid value pass', () => {
-    expect(rule1Evaluation(test_data.rule1.test7, 50)).toEqual({permit: true, rationale: null});
+    // test if function fails if no bid value given
+    test('no bid value pass', () => {
+        const testData = Object.assign({}, data, {bid: {"type": "Accept"}});
+        expect(rule1Evaluation(testData, 50)).toEqual({permit: true, rationale: null});
+    });
 });
 
 // RULE4EVALUATION
 // Test if message.len < 100 passes
+// test('rule1 len < 100 pass', () => {
+//     expect(rule4Evaluation(test_data.rule4.test1)).toEqual({permit: true, rationale: null});
+// });
 
-// Test if message.len === 100 fails
+// // Test if message.len === 100 fails
+// test('rule1 len = 100 pass', () => {
+//     expect(rule4Evaluation(test_data.rule4.test2)).toEqual({permit: true, rationale: null});
+// });
 
-// Test if message.len > 100 fails
+// // Test if message.len > 100 fails
+// test('rule1 len > 100 pass', () => {
+//     expect(rule4Evaluation(test_data.rule4.test3)).toEqual({permit: false, rationale: "Excessive message length"});
+// });
 
-// Test if function fails without ['text'] given
-
-// test if function fails if ['text'] is non-alphabetic
+// // Test if function passes without ['text'] given
+// test('rule1 no text given pass', () => {
+//     expect(rule4Evaluation(test_data.rule4.test4)).toEqual({permit: true, rationale: null});
+// });
